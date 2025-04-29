06cb5879f437724c5432f39c3b3d7dc07bb1e36b
Eren <erenncitak@gmail.com>
2025-04-29 17:14:53 +0300

    Infinite Scroll Image Gallery
    Unsplash API kullanılarak geliştirilen bu projede, HTML, CSS ve JavaScript ile dinamik bir resim galerisi oluşturulmuştur. Sonsuz kaydırma (infinite scroll) özelliği sayesinde kullanıcı sayfanın altına ulaştıkça yeni görseller otomatik olarak yüklenir ve animasyonlu bir yükleyiciyle desteklenir.

---
+++ Infinite-Scroll-Image-Gallery/script.js
@@ -0,0 +1,64 @@
+const count = 30;
+const apiKey = 'P-2Jc9gTMyWPXGjCJE7pUzTgN0fyc1wKopoT1rjQ5Ao';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+const imageContainer = document.getElementById('imageDiv');
+const loader = document.getElementById('loading');
+
+let imagesArray = [];
+let isDownloaded=false;
+let imagesLoaded = 0;
+let totalImages = 0;
+
+
+getImages();
+async function getImages() {
+    try {
+        const response = await fetch(apiUrl);
+        imagesArray = await response.json();
+        // console.log(imagesArray);
+        displayImages();
+    }
+    catch (error) {
+    }
+}
+
+function displayImages() {
+    imagesLoaded = 0;
+    totalImages = imagesArray.length;
+    imagesArray.forEach((image) => {
+        const item = document.createElement('a');
+        setAttribute(item, { href: image.urls.regular });
+        // item.setAttribute('href', image.urls.regular);
+        const img = document.createElement('img');
+        // img.setAttribute('src', image.urls.regular);
+        // img.setAttribute('alt', image.alt_description);
+        setAttribute(img, {
+            src: image.urls.regular,
+            alt: image.alt_description,
+        });
+        img.addEventListener('load', imageLoaded);
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        isDownloaded = true;
+        loader.hidden = true;
+    }
+}
+
+function setAttribute(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+window.addEventListener('scroll', () => {
+    console.log("Tetiklendi");
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && isDownloaded) {
+        getImages();
+    }
+});
